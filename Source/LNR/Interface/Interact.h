60a4c6faf9f18f3c42f7df21611bc6aee9cafa16
esquilofreniko <esquilofreniko@gmail.com>
2022-04-26 10:47:33 +0100

    Updated interface

---
+++ Source/LNR/Interface/Interact.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "Interact.generated.h"
+
+UINTERFACE(BlueprintType, Blueprintable)
+class UInteract : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class LNR_API IInteract
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	void OnInteract(class AHero* hero);
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	void OnShowInfo(class AHero* hero, bool val);
+};
