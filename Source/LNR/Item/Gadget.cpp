0f07053943cf01eb9d6a3e00e8a77d6697bbb9b7
esquilofreniko <esquilofreniko@gmail.com>
2022-06-11 03:36:04 +0100

    Fixes to Gadgets

--- Source/LNR/Item/Gadget.cpp
+++ Source/LNR/Item/Gadget.cpp
@@ -1,11 +1,10 @@
 #include "Gadget.h"
 #include "LNR/Body/Body.h"
 #include "LNR/Gadget/GadgetBase.h"
-#include "XProjectileSBOT.h"
 
 void UGadget::Consume_Implementation(ABody* body)
 {
-	FVector loc = body->GetMesh()->GetSocketLocation("RightHand");
+	const FVector loc = body->GetMesh()->GetSocketLocation("RightHand");
 	AGadgetBase* gb = Cast<AGadgetBase>(body->SpawnActor(Gadget, loc, FRotator()));
 	gb->ActivateGadget(body, loc);
 }
